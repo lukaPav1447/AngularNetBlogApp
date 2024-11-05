2a88ef7499464976e4b0fc35f1960bd8baf3e50b
pavlovicl <pavlovicl@PAVLOVICL-LAP>
2024-11-05 16:08:54 +0100

    DbContext and Migration

---
+++ API/AngularNetBlogApp.API/AngularNetBlogApp.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AngularNetBlogApp.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularNetBlogApp.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
