b1b2f6d66ea2dc452c2a4367551707942ab5003a
pavlovicl <pavlovicl@PAVLOVICL-LAP>
2024-11-25 19:46:49 +0100

    Register and Login action methods

---
+++ API/AngularNetBlogApp.API/AngularNetBlogApp.API/Controllers/AuthController.cs
@@ -0,0 +1,100 @@
+﻿using AngularNetBlogApp.API.Models.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularNetBlogApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+        {
+            //Check Email
+            var identityUser = await userManager.FindByEmailAsync(request.Email);
+
+            if (identityUser is not null)
+            {
+                //Check Password
+                var checkPasswordResult = await userManager.CheckPasswordAsync(identityUser, request.Password);
+
+                if (checkPasswordResult)
+                {
+                    var roles = await userManager.GetRolesAsync(identityUser);
+
+                    //Create a Token and Response
+
+
+                    var response = new LoginResponseDto()
+                    {
+                        Email = request.Email,
+                        Roles = roles.ToList(),
+                        Token = "TOKEN"
+                    };
+
+                    return Ok(response);
+                }
+            }
+            ModelState.AddModelError("", "Email or Password Incorrect");
+
+            return ValidationProblem(ModelState);
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
+        {
+            //Create IdentityUser object
+
+            var user = new IdentityUser
+            {
+                UserName = request.Email?.Trim(),
+                Email = request.Email?.Trim()
+            };
+
+            //Create User
+            var identityResult = await userManager.CreateAsync(user, request.Password);
+
+            if (identityResult.Succeeded)
+            {
+                //Add role to user (Reader)
+                identityResult = await userManager.AddToRoleAsync(user, "Reader");
+
+                if (identityResult.Succeeded)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    if (identityResult.Errors.Any())
+                    {
+                        foreach (var error in identityResult.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (identityResult.Errors.Any())
+                {
+                    foreach (var error in identityResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return ValidationProblem(ModelState);
+        }
+    }
+}
