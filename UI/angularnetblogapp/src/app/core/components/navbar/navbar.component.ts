c17da7cfc8ce998b3c224e7f499c3de219fd4c20
pavlovicl <pavlovicl@PAVLOVICL-LAP>
2024-11-27 08:54:43 +0100

    stavljanje tokena u local storage

--- UI/angularnetblogapp/src/app/core/components/navbar/navbar.component.ts
+++ UI/angularnetblogapp/src/app/core/components/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../features/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {
+    this.authService.user().subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+    });
+  }
 }
--- UI/angularnetblogapp/src/app/features/auth/login/login.component.ts
+++ UI/angularnetblogapp/src/app/features/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { LoginRequest } from '../models/login-request.model';
 import { AuthService } from '../services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,11 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent {
   model: LoginRequest;
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private cookieService: CookieService,
+    private router: Router
+  ) {
     this.model = {
       email: '',
       password: '',
@@ -20,7 +26,25 @@ export class LoginComponent {
   onFormSubmit(): void {
     this.authService.login(this.model).subscribe({
       next: (response) => {
-        console.log(response);
+        //Set Auth Cookie
+        this.cookieService.set(
+          'Authorization',
+          `Bearer ${response.token}`,
+          undefined,
+          '/',
+          undefined,
+          true,
+          'Strict'
+        );
+
+        //Set User
+        this.authService.setUser({
+          email: response.email,
+          roles: response.roles,
+        });
+
+        //Redirect back to Home
+        this.router.navigateByUrl('/');
       },
     });
   }
